671b4157c92f9248f300e276a6715e47ebb45cde
Bartek Nowak <barteknowak31l@gmail.com>
2024-02-18 18:56:22 +0100

    api rework

--- src/Controller/ApiPokemonController.php
+++ src/Controller/ApiPokemonController.php
@@ -23,156 +23,129 @@ use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
 class ApiPokemonController extends AbstractController
 {
 
-    private const RESPONSE_ALL_WERE_GUESSED = 'Congratulations! All pokemons from this generation were guessed!';
-    private const RESPONSE_OK = 'Ok';
-    private const RESPONSE_CORRECT_ANSWER = 'Correct Answer!';
-    private const RESPONSE_WRONG_ANSWER = 'Wrong Answer!';
-    private const RESPONSE_WRONG_ID = 'Wrong id!';
-    private const RESPONSE_MISSING_DATA = 'Missing data!';
-    private const RESPONSE_SOMETHING_WENT_WRONG = 'Opps, something went wrong on the server :/ please contact our support';
-
-
-
 
     #[Route('/api/pokemon/{generation<\d+>?1}', name: 'app_api_pokemon', methods: ['GET'])]
-    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function getRandomPokemon(
-        #[CurrentUser] User $user,
         int $generation,
         PokemonRepository $pokemonRepository,
-        EntityManagerInterface $em
+        #[CurrentUser] User $user,
+        EntityManagerInterface $em,
     ): JsonResponse {
 
-        $pokemons = $user->getPokemonsByGeneration($generation);
+
         $user->setGeneration($generation);
-        $randomId = $user->getRandomPokeIdThatWasNotGuessedBefore($pokemons);
+        $randomId = $user->getRandomPokeIdThatWasNotGuessedBefore($user->getPokemons());
 
-        $message = $randomId === User::ALL_WERE_GUESSED_CODE ? ApiPokemonController::RESPONSE_ALL_WERE_GUESSED : ApiPokemonController::RESPONSE_OK;
-
-        // we also need types, img source and encoded name
-
-        if ($randomId !== User::ALL_WERE_GUESSED_CODE) {
-
-
-            // try fetch pokemon form db
-            $pokemon = $pokemonRepository->find($randomId);
-            if ($pokemon instanceof Pokemon) {
-                // all good we got a pkmn
-            } else {
-                // call poke api
-                $pokemon = HelloController::callPokeApi($em, $randomId, $user);
-            }
-
-            $response =
-                [
-                    'id' => $pokemon->getId(),
-                    'type1' => $pokemon->getType1(),
-                    'type2' => $pokemon->getType2(),
-                    'img' => $pokemon->getSpriteUrl(),
-                ];
-
-            // set last asked id on the user
-            $user->setLastAskedId($randomId);
-            $em->persist($user);
-            $em->flush();
+        // try fetch pokemon form db
+        $pokemon = $pokemonRepository->find($randomId);
+        if ($pokemon instanceof Pokemon) {
+            // all good we got a pkmn
+        } else {
+            // call poke api
+            $pokemon = HelloController::callPokeApi($em, $randomId, new User());
         }
+
+        $response =
+            [
+                'id' => $pokemon->getId(),
+                'name' => $pokemon->getName(),
+                'type1' => $pokemon->getType1(),
+                'type2' => $pokemon->getType2(),
+                'img' => $pokemon->getSpriteUrl(),
+            ];
 
 
         return $this->json([
-            'message' => $message,
-            'generation' => $generation,
             'response' =>  $randomId === User::ALL_WERE_GUESSED_CODE ? null : $response,
         ]);
     }
 
 
-    #[Route('/api/pokemon/answer', name: 'app_api_pokemon_answer', methods: ['POST'])]
-    #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    public function checkAnswer(
-        #[CurrentUser] User $user,
+
+    #[Route('/api/pokemon/add/{id<\d+>}', name: 'app_api_pokemon_add', methods: ['POST'])]
+    public function addPokemon(
+        int $id = null,
         PokemonRepository $pokemonRepository,
+        #[CurrentUser] User $user,
         EntityManagerInterface $em,
-        Request $request
     ): JsonResponse {
 
 
-        if ($request->getPayload()->has('id')) {
-            $id = $request->getPayload()->get('id');
-        } else {
-            return $this->json([
-                'message' => ApiPokemonController::RESPONSE_MISSING_DATA,
-            ]);
-        }
+        $request_ok = false;
 
-
-        if ($request->getPayload()->has('answer')) {
-            $answerName = strtolower($request->getPayload()->get('answer'));
-        } else {
-            return $this->json([
-                'message' => ApiPokemonController::RESPONSE_MISSING_DATA,
-            ]);
-        }
-
-
-        // check if last asked id is the id user provided answer for
-        if ($id != $user->getLastAskedId() || $id == User::LAST_ASKED_DEFAULT_VALUE) {
-            return $this->json([
-                'message' => ApiPokemonController::RESPONSE_WRONG_ID,
-            ]);
-        }
-
-        // check if answer is correct
+        // try fetch pokemon form db
         $pokemon = $pokemonRepository->find($id);
+        if ($pokemon instanceof Pokemon) {
 
-        // if (null === $pokemon) {
-        //     return $this->json([
-        //         'message' => ApiPokemonController::RESPONSE_WRONG_ID,
-        //     ]);
-        // }
 
-        if ($pokemon->getName() == $answerName) {
-            // correct
-            $user->addPokemon($pokemon);
-            $user->setLastAskedId(User::LAST_ASKED_DEFAULT_VALUE);
-            $em->persist($user);
-            $em->flush();
+            if (!$user->getPokemons()->contains($pokemon)) {
+                $user->addPokemon($pokemon);
+                $em->persist($user);
+                $em->flush();
+                $request_ok = true;
+            }
+        }
 
-            return $this->json([
-                'message' => ApiPokemonController::RESPONSE_CORRECT_ANSWER
-            ]);
+        if ($request_ok) {
+            $response =
+                [
+                    "message" => 'pokemon added'
+                ];
         } else {
-
-            return $this->json([
-                'message' => ApiPokemonController::RESPONSE_WRONG_ANSWER
-            ]);
+            $response =
+                [
+                    "message" => 'error'
+                ];
         }
 
 
         return $this->json([
-            'message' => ApiPokemonController::RESPONSE_SOMETHING_WENT_WRONG
+            'response' =>  $response,
         ]);
     }
 
 
-    #[Route('/api/pokemon/all/{generation}', name: "app_api_pokemon_all", methods: ['GET'])]
-    #[IsGranted("IS_AUTHENTICATED_FULLY")]
-    public function getAllUserPokemons(#[CurrentUser] User $user, int $generation = null): JsonResponse
-    {
+    #[Route('/api/pokemon/all/{generation<\d+>}', name: 'app_api_pokemon_all', methods: ['GET'])]
+    public function getAllOwnedPokemonFromGeneration(
+        int $generation = null,
+        #[CurrentUser] User $user,
+    ): JsonResponse {
 
-        if (null !== $generation) {
-            return $this->json(
-                [
-                    'message' => 'ok',
-                    'list' => $user->getPokemonsByGeneration($generation)
-                ]
-            );
+
+        $request_ok = false;
+        if ($generation >= 1 && $generation <= 9) {
+            $request_ok = true;
         }
 
-        return $this->json(
-            [
-                'message' => 'ok',
-                'list' => $user->getPokemons()
-            ]
-        );
+
+        if ($request_ok) {
+
+            $pokemons = $user->getPokemonsByGeneration($generation);
+
+            // if ($pokemons->count() == 1)
+            //     $pokemons = [$pokemons->first()];
+            // else if ($pokemons->count() == 0)
+            //     $pokemons = [];
+
+            $pokeList = [];
+            foreach ($pokemons as $pkmn) {
+                array_push($pokeList, $pkmn);
+            }
+
+            $response =
+                [
+                    "message" => $pokeList
+                ];
+        } else {
+            $response =
+                [
+                    "message" => 'error'
+                ];
+        }
+
+
+        return $this->json([
+            'response' =>  $response,
+        ]);
     }
 }
